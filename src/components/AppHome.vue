f79499b1f0681178a026806616563f5955e4668f
Vinnie Pauley <vinniepauley@gmail.com>
2023-03-25 11:36:32 -0500

    added bootstrap design system and a couple of example components

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -3,14 +3,27 @@
     <h1>Home</h1>
     <p>{{todoCount}}</p>
     <p>{{ count }}</p>
-    <button @click="addNewTodo">Add Todo</button>
+    <p>{{text}}</p>
+    <b-form-textarea
+      id="textarea"
+      v-model="text"
+      placeholder="Enter something..."
+      rows="3"
+      max-rows="6"
+    ></b-form-textarea>
+    <b-button variant="outline-primary" @click="addNewTodo">Add</b-button>
   </div>
 </template>
 
 <script>
+import { BButton, BFormTextarea } from "bootstrap-vue";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
+  components: { BButton, BFormTextarea },
+  data: () => ({
+    text: ""
+  }),
   methods: {
     ...mapActions({
       addTodo: "Todos/addTodo"
